bedc4f6ec7330207908be07c39f9de7ba707e7fa
Faiyaz Tanvee <faiyaztanvee009@gmail.com>
2025-07-19 20:28:07 +0600

    Add dotenv and configure CORS for frontend
    Introduced dotenv for environment variable management and updated server to use process.env.PORT. Configured CORS to allow requests only from the live frontend at https://intern-task-ashy.vercel.app for improved security.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,23 @@
-
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import eventRoutes from './routes/eventRoutes';
+import 'dotenv/config'; 
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
-app.use(cors());
+// Specific CORS configuration for my live frontend
+const corsOptions = {
+  origin: 'https://intern-task-ashy.vercel.app',
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
-// Telling Express to use our event routes for any request to /api/events
 app.use('/api/events', eventRoutes);
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
